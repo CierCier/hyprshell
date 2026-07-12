53403c5b2d6eb70d5bef50d198f24819f9faed02
Aabish Malik <aabishmalik3337@gmail.com>
2026-07-12 18:47:58 +0530

    bump 2026-07-12

--- config/fish/conf.d/30_fzf.fish
+++ config/fish/conf.d/30_fzf.fish
@@ -1,3 +1,4 @@
 if command -q fzf
     fzf --fish | source
 end
+
--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,3 +1,7 @@
 if status is-interactive
     set -g fish_greeting
 end
+
+# >>> grok installer >>>
+fish_add_path $HOME/.grok/bin
+# <<< grok installer <<<
